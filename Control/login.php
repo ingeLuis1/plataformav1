7644f9caa37b7d51a95a83a66f6e8b7c89637c47
LUIS <luis.gf@sfelipeprogreso.tecnm.mx>
2025-02-01 12:17:46 -0600

    inicioSesion_avanzado

--- Control/login.php
+++ Control/login.php
@@ -3,12 +3,12 @@ include('conexionBD.php');
 session_start(); ///Forma de almacenar informacion del usuario en el servidor 
 
 $pdo = conectarBD();
-
-$sql = "SELECT * FROM `usuarios` WHERE e_mail=:mail and contraseña=:contra";
+$sql = "SELECT * FROM `usuarios` WHERE email=:mail and contra=:contra";
 $sql = $pdo->prepare($sql);
 $sql->bindParam("mail", $username);
 $sql->bindParam("contra", $password);
 $sql->execute();
+echo("ddd");
 ///Obtener los datos de la consulta
 $usuario = $sql->fetch(PDO::FETCH_ASSOC);
 ///Trabajar con los datos de la consulta
@@ -16,27 +16,30 @@ if ($usuario) {
     ///Guardar datos de session
     $_SESSION["rol"] = $usuario["rol"];
     $_SESSION["id_usuario"] = $usuario["id_usuario"];
-    
-
     ///redirigir a la pagina adecuada por cada rol
     ///
     if ($usuario["rol"] == "USER") {
         ///vista de usuario
-        header("Location: ../Views/index.html?success=user");
+        header("Location: ../index.html?success=user");
 
     } else if ($usuario["rol"] == "ADMIN") {
         /////Vista de administrador
-        header("Location: ../Views/index.html?success=admin");
+        header("Location: ../index.html?success=admin");
 
     } else if ($usuario["rol"] == "ACADEMICO") {
         ///Vista de Academico
-        header("Location: ../Views/index.html?success=academico");
+        header("Location: ../index.html?success=academico");
     }else if ($usuario["rol"] == "JEFE DE DIVISION") {
         ///Vista de Academico
-        header("Location: ../Views/index.html?success=jefe de division");
+        header("Location: ../index.html?success=jefe de division");
 
 } else {
+    ///Regresar al Login con los codigos de eerror para mostrar el error
+    header("Location:../Views/login.html?error=1");
+}
+}
+else {
     ///Regresar al Login
-    header("Location:../Views/index.html?error=1");
-}}
+    header("Location:../Views/login.html?error=1");
+}
 ?>

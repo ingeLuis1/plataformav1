f848abda6e8e8ef48a4cb3dd05f4c4210c8a9ee6
LUIS <luis.gf@sfelipeprogreso.tecnm.mx>
2025-02-03 01:16:52 -0600

    actualizacion

--- Control/login.php
+++ Control/login.php
@@ -5,9 +5,7 @@ session_start(); ///Forma de almacenar informacion del usuario en el servidor
 $email = $_POST["user"];
 $contra = $_POST["pass"];
 
-
 $pdo = conectarBD();
-
 
 $sql = "SELECT * FROM `usuarios` WHERE email=:mail ";
 $sql = $pdo->prepare($sql);
@@ -18,7 +16,8 @@ $usuario = $sql->fetch(PDO::FETCH_ASSOC);
 ///Trabajar con los datos de la consulta
 if ($usuario) {
     $contrabd = $usuario["contra"];///obtiene la contra del usuario en laBD
-    if (password_verify($contra, $contrabd)) {///Verifica que la contra sea igual a la de la BD
+    if (password_verify($contra, $contrabd)) {
+        ///Verifica que la contra sea igual a la de la BD
         ///Guardar datos de session
         $_SESSION["rol"] = $usuario["rol"];
         $_SESSION["id_usuario"] = $usuario["id_usuario"];
@@ -26,13 +25,13 @@ if ($usuario) {
         ///
         if ($usuario["rol"] == "egresado") {
             ///vista de usuario
-            header("Location: ../Views/login.htlm?success=egresado");
+            header("Location: ../Views/login.html?success=egresado");
 
         } else if ($usuario["rol"] == "empleador") {
             /////Vista de administrador
-            header("Location: ../Views/login.htlm?success=empleador");
+            header("Location: ../Views/login.html?success=empleador");
 
-        } 
+        }
         // else if ($usuario["rol"] == "ACADEMICO") {
         //     ///Vista de Academico
         //     header("Location: ../index.html?success=academico");
@@ -45,6 +44,9 @@ if ($usuario) {
             ///Regresar al Login con los codigos de eerror para mostrar el error datos incorrectos
             header("Location:../Views/login.html?error=1");
         }
+    } else {
+        ///Regresar al Login con los codigos de eerror para mostrar el error datos incorrectos
+        header("Location:../Views/login.html?error=1");
     }
 
 } else {

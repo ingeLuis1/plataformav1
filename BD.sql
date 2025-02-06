cfdc674c1b24a4da1f50ba0714a446533698f11c
LUIS <luis.gf@sfelipeprogreso.tecnm.mx>
2025-02-06 15:17:19 -0600

    actualizacion

--- BD.sql
+++ BD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 04-02-2025 a las 05:16:18
+-- Tiempo de generación: 06-02-2025 a las 22:16:31
 -- Versión del servidor: 8.1.0
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `controlrespuestas`
+--
+
+CREATE TABLE `controlrespuestas` (
+  `id_control` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `status` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `cuestionarios`
 --
 
@@ -31,7 +43,7 @@ CREATE TABLE `cuestionarios` (
   `id_cuestionario` int NOT NULL,
   `fechaCreacion` date NOT NULL,
   `tipo` enum('atributos','objetivos') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -48,17 +60,18 @@ CREATE TABLE `datos` (
   `id_usuario` int NOT NULL,
   `cohorte` varchar(255) NOT NULL,
   `empresa` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `datos`
 --
 
 INSERT INTO `datos` (`id_datos`, `nombre`, `apellidoP`, `apellidoM`, `telefono`, `id_usuario`, `cohorte`, `empresa`) VALUES
-(4, 'Luis Angel', 'Gonzalez', 'Flores', '123', 9, 'II-503', 'TESSFP'),
-(5, 'Luis Angel', 'Gonzalez', 'Flores', '741852', 10, '2025-2020', 'Pollotec'),
-(6, 'Luis Angel', 'Gonzalez', 'Flores', '741852', 10, '2025-2020', 'Pollotec'),
-(7, 'Luis Angel', 'Gonzalez', 'Flores', '3423', 11, 'II-502', 'TESSFP');
+(4, 'Luis Angel', 'Gonzalez', 'Flores', '123', 9, '2020-2025', 'TESSFP'),
+(6, 'Luis Angel', 'Gonzalez', 'Flores', '741852', 10, '2020-2025', 'Pollotec'),
+(8, 'Luis Angel', 'Gonzalez', 'Flores', '1234', 12, '2014-2018', ''),
+(9, 'd', 'd', 'd', '333', 13, '2010-2014', ''),
+(10, 'k', 'k', 'kk', '222', 14, '', 'tesfp');
 
 -- --------------------------------------------------------
 
@@ -71,7 +84,7 @@ CREATE TABLE `opciones` (
   `etiqueta` varchar(2) NOT NULL,
   `opcion` varchar(255) NOT NULL,
   `id_pregunta` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -83,7 +96,7 @@ CREATE TABLE `preguntas` (
   `id_pregunta` int NOT NULL,
   `pregunta` varchar(255) NOT NULL,
   `id_cuestionario` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -96,7 +109,7 @@ CREATE TABLE `respuestas` (
   `id_usuario` int NOT NULL,
   `id_pregunta` int NOT NULL,
   `id_opcion` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -107,9 +120,9 @@ CREATE TABLE `respuestas` (
 CREATE TABLE `usuarios` (
   `id_usuario` int NOT NULL,
   `email` varchar(100) NOT NULL,
-  `contra` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `contra` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `rol` enum('egresado','admin','empleador','user') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -118,11 +131,20 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id_usuario`, `email`, `contra`, `rol`) VALUES
 (9, 'lui1525angel@gmail.com', '$2y$10$9cTuIwmFZbV6lfCC5Si4he0r6n40Zak7IstKRcQ1g8bCYGOhkusZm', 'egresado'),
 (10, 'lui1525', '$2y$10$9cTuIwmFZbV6lfCC5Si4he0r6n40Zak7IstKRcQ1g8bCYGOhkusZm', 'admin'),
-(11, 'lui', '$2y$10$B5f.8wJfVVb8uBUHx6anxOcxzSR8opZRS2SdjnjTENAVTeAQ1bwVe', 'admin');
+(11, 'lui', '$2y$10$B5f.8wJfVVb8uBUHx6anxOcxzSR8opZRS2SdjnjTENAVTeAQ1bwVe', 'admin'),
+(12, 'lui1525angel@gmail.com', '$2y$10$fWWazxLpHTB/gVmB/JPn6OroBtsDN8MYve0xKofGBQ/W0PqSHghIa', 'egresado'),
+(13, 'juan', '$2y$10$5CEDzRigLS5.ibf6AGGyHe11gA7S7Gm1H6Oy4s/CHzW2TJP7tm3Te', 'egresado'),
+(14, 'inge', '$2y$10$mhvY33VxDoBYMzLPBpKiM.Cnse9Jg1yFvLt.7NC7yySuLhsAnzoUK', 'empleador');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `controlrespuestas`
+--
+ALTER TABLE `controlrespuestas`
+  ADD PRIMARY KEY (`id_control`);
 
 --
 -- Indices de la tabla `cuestionarios`
@@ -171,28 +193,34 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `controlrespuestas`
+--
+ALTER TABLE `controlrespuestas`
+  MODIFY `id_control` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `cuestionarios`
 --
 ALTER TABLE `cuestionarios`
-  MODIFY `id_cuestionario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id_cuestionario` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `datos`
 --
 ALTER TABLE `datos`
-  MODIFY `id_datos` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_datos` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `opciones`
 --
 ALTER TABLE `opciones`
-  MODIFY `id_opcion` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=631;
+  MODIFY `id_opcion` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `preguntas`
 --
 ALTER TABLE `preguntas`
-  MODIFY `id_pregunta` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=213;
+  MODIFY `id_pregunta` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `respuestas`
@@ -204,7 +232,7 @@ ALTER TABLE `respuestas`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_usuario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Restricciones para tablas volcadas
